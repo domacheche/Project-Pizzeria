d1004894ffa22883c47657e7985bf5a9ea9cd6d3
domacheche <bochenekdoma@gmail.com>
2024-01-25 15:05:07 +0100

    zadanie handlabars

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,10 +1,13 @@
 export const select = {
     templateOf: {
       menuProduct: "#template-menu-product",
+      bookingWidget: '#template-booking-widget',
     },
     containerOf: {
       menu: '#product-list',
       cart: '#cart',
+      pages: '#pages',
+      booking: '.booking-wrapper',
     },
     all: {
       menuProducts: '#product-list > .product',
@@ -25,30 +28,42 @@ export const select = {
         linkDecrease: 'a[href="#less"]',
         linkIncrease: 'a[href="#more"]',
       },
+      datePicker: {
+        wrapper: '.date-picker',
+        input: `input[name="date"]`,
     },
+    hourPicker: {
+        wrapper: '.hour-picker',
+        input: 'input[type="range"]',
+        output: '.output',
+    },
+},
+booking: {
+    peopleAmount: '.people-amount',
+    hoursAmount: '.hours-amount',
+    tables: '.floor-plan .table',
+},
+nav: {
+    links: '.main-nav a',
+},
 
-    cartProduct: {
+cartProduct: {
       amountWidget: '.widget-amount',
       price: '.cart__product-price',
       edit: '[href="#edit"]',
       remove: '[href="#remove"]',
     },
 
-    cart: {
+cart: {
       productList: '.cart__order-summary',
       toggleTrigger: '.summary',
       totalNumber: `.cart__total-number`,
       totalPrice: '.cart__total-price .cart__order-total',
       subtotalPrice: '.cart__order-subtotal .cart__order-price-sum',
       deliveryFee: '.cart__order-delivery .cart__order-price-sum',
+      form: '.cart__order',
 
-    },
-
-    
-    
-
-    
-  };
+}};
 
   export const classNames = {
     menuProduct: {
@@ -58,6 +73,16 @@ export const select = {
      cart: {
        wrapperActive: 'active',
      },
+     booking: {
+        loading: 'loading',
+        tableBooked: 'booked',
+    },
+    nav: {
+        active: 'active',
+    },
+    pages: {
+        active: 'active',
+    }
   };
 
   export const settings = {
@@ -75,6 +100,22 @@ export const select = {
       url: '//localhost:3131',
       products: 'products',
       orders: 'orders',
+      bookings: 'bookings',
+      events: 'events',
+      dateStartParamKey: 'date_gte',
+      dateEndParamKey: 'date_lte',
+      notRepeatParam: 'repeat=false',
+      repeatParam: 'repeat_ne=false',
+    },
+    hours: {
+        open: 12,
+        close: 24,
+    },
+    datePicker: {
+        maxDaysInFuture: 14,
+    },
+    booking: {
+        tableIdAttribute: 'data-table',
     },
 
   };
@@ -84,4 +125,5 @@ export const select = {
   export const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
     cartProduct: Handlebars.compile(document.querySelector('#template-cart-product').innerHTML),
+    bookingWidget: Handlebars.compile(document.querySelector(select.templateOf.bookingWidget).innerHTML),
   };
